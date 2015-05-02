df3e00fdbfc6a3e0a4640490c5727cc5f8e68425
Vicente Franco <vfranco@roshka.com>
2015-05-02 15:26:45 -0400

    Agregamos un manejador de error simple, vamos a ver luego para mejorar.

---
+++ app/Helpers/Err.php
@@ -0,0 +1,31 @@
+<?php
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Err
+ *
+ * @author vfranco
+ */
+class Err {
+    const ERROR_RESPONSE = 'error';
+    private $code;
+    private $description;
+    
+    function __construct($code, $description){
+        $this->code = $code;
+        $this->description = $description;  
+    }
+
+
+    static function getErrorFromDescription($code, $description){
+        $error = new Err($code, $description);
+        $response = array(ERROR_RESPONSE => $error);
+        return $response;
+    }
+}
