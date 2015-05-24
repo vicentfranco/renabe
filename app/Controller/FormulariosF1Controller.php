98fd926deac64dd1bdf33a0cda3136e09a77bdf6
iarmoa <isaias.armoa@gmail.com>
2015-05-23 20:17:03 -0400

    Ya anda yaaa!! No fue por idiota

--- app/Controller/FormulariosF1Controller.php
+++ app/Controller/FormulariosF1Controller.php
@@ -26,13 +26,13 @@ class FormulariosF1Controller extends AppController {
     public function addHeader(){
     	if($this->request->is('post')){
     		$reply = array();
-    		if(!empty($this->data)){
-    			$this->data['FormularioF1']['fecha_inicio'] = $this->data['FormularioF1']['fecha_inicio'].'-07-01';
-                $this->data['FormularioF1']['fecha_fecha'] =  $this->data['FormularioF1']['fecha_fin'].'-07-01';
+    		if(!empty($this->request->data)){
+                $this->request->data['FormularioF1']['fecha_inicio'] = $this->request->data['FormularioF1']['fecha_inicio'].'-07-01';
+                $this->request->data['FormularioF1']['fecha_fin'] =  $this->request->data['FormularioF1']['fecha_fin'].'-07-01';
     		}else{
     			return $reply = array('status'=>'error', 'message'=>'Not input data');
     		}
-    		if($this->FormularioF1->saveAll($this->data)){
+    		if($this->FormularioF1->saveAll($this->request->data['FormularioF1'])){
     			return $this->responseJson(array('status'=>'ok', 'message'=>$this->FormularioF1->id));
     		}else{
     			return $this->responseJson(array('status'=>'error', 'message'=>'Error saving'));
